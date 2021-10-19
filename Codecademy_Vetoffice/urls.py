d6848f546c8d9861225f086f769a6951d4d61b4a
Jørgen <jorgen.s.baumann@gmail.com>
2021-10-19 13:06:07 +0200

    Basic website work on runserver, http://127.0.0.1:8000/vetoffice/

--- Codecademy_Vetoffice/urls.py
+++ Codecademy_Vetoffice/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('vetoffice', include('vetoffice.urls')),
+    path('vetoffice/', include('vetoffice.urls')),
+    path('', lambda request: HttpResponse('This response worked!')),
 ]
